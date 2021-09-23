215b3ccd5dfb2060fb52dd5787b6cf3ba704bd4b
wyntau <wyntau@outlook.com>
2021-09-23 23:40:31 +0800

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     }
   },
   env: {
-    browser: true,
     node: true,
     es6: true
   },
@@ -22,33 +21,5 @@ module.exports = {
     'plugin:prettier/recommended'
   ],
   plugins: ['@typescript-eslint'],
-  rules: {
-    /**
-     * eslint-rules
-     */
-    'valid-jsdoc': 'warn',
-    'array-bracket-spacing': ['error', 'never'],
-    'no-case-declarations': 'off',
-    'comma-dangle': 'off',
-
-    /**
-     * @typescript-eslint rules
-     */
-    '@typescript-eslint/array-type': ['error', {default: 'generic'}],
-    '@typescript-eslint/ban-ts-comment': 'warn',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        "selector": "interface",
-        "format": ["PascalCase"],
-        "prefix": ["I"]
-      }
-    ],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off'
-  }
+  rules: {}
 };
