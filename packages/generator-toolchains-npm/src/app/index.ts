4a745fb126c1c16e8230ca43d17ab43d987dbf1c
wyntau <github@wtliu.com>
2021-09-24 15:23:49 +0800

    feat: add npmrc

---
+++ packages/generator-toolchains-npm/src/app/index.ts
@@ -0,0 +1,20 @@
+import Generator from 'yeoman-generator';
+
+export default class extends Generator {
+  props: Record<string, any> = {};
+
+  async prompting(): Promise<void> {
+    this.props = await this.prompt([
+      {
+        type: 'confirm',
+        name: 'toolchains.npm.enable',
+        message: '添加 npm 配置文件',
+        default: true,
+      },
+    ]);
+  }
+
+  writing(): void {
+    this.fs.copy(this.templatePath('.npmrc'), this.destinationPath('.npmrc'));
+  }
+}
