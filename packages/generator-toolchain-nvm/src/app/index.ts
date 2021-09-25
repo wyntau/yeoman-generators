cf8a41ef5a400e26ea03d2b06e6a2f8da0285697
wyntau <github@wtliu.com>
2021-09-25 11:19:32 +0800

    refactor(nvm): remove useless code

--- packages/generator-toolchain-nvm/src/app/index.ts
+++ packages/generator-toolchain-nvm/src/app/index.ts
@@ -1,19 +1,6 @@
 import Generator from 'yeoman-generator';
 
 export default class extends Generator {
-  props: Record<string, any> = {};
-
-  async prompting(): Promise<void> {
-    this.props = await this.prompt([
-      {
-        type: 'confirm',
-        name: 'toolchain.nvm.enable',
-        message: '添加 nvm 配置文件',
-        default: true,
-      },
-    ]);
-  }
-
   writing(): void {
     this.fs.copy(this.templatePath('.nvmrc'), this.destinationPath('.nvmrc'));
   }
