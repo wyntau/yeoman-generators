60cbc1abc49ed97f4524afbaa5d5faf95a5687f3
wyntau <github@wtliu.com>
2021-09-25 16:52:03 +0800

    feat: add commitlint and lint-staged

---
+++ packages/generator-toolchain-commitlint/src/app/index.ts
@@ -0,0 +1,11 @@
+import Generator from 'yeoman-generator';
+import { IGeneratorOptions } from '@wyntau/generator-shared';
+
+export default class extends Generator<IGeneratorOptions> {
+  async writing(): Promise<void> {
+    this.fs.copy(this.templatePath('.'), this.destinationPath('.'), {
+      globOptions: { dot: true },
+    });
+    await this.addDevDependencies(['@commitlint/cli', '@commitlint/config-conventional']);
+  }
+}
