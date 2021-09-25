b484795e14debd26fbe87355c7b82ce36258e168
wyntau <github@wtliu.com>
2021-09-25 17:10:52 +0800

    feat: add generator-typescript

---
+++ packages/generator-toolchain-typescript/src/app/index.ts
@@ -0,0 +1,14 @@
+import Generator from 'yeoman-generator';
+import chalk from 'chalk';
+import { IGeneratorOptions } from '@wyntau/generator-shared';
+
+export default class extends Generator<IGeneratorOptions> {
+  async writing(): Promise<void> {
+    await this.addDevDependencies(['typescript']);
+    await this.addDependencies(['tslib']);
+  }
+
+  end(): void {
+    this.log(['', `✨  请通过 \`${chalk.red('npx tsc --init')}\` 初始化 tsconfig.json`, ''].join('\n'));
+  }
+}
