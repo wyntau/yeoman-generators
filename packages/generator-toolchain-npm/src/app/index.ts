118216907e20b662f6b06e27e89526cab0e0edeb
wyntau <github@wtliu.com>
2021-09-25 10:47:07 +0800

    refactor(npm): 添加提示信息

--- packages/generator-toolchain-npm/src/app/index.ts
+++ packages/generator-toolchain-npm/src/app/index.ts
@@ -1,7 +1,17 @@
 import Generator from 'yeoman-generator';
+import chalk from 'chalk';
 
-export default class extends Generator {
+export interface IOptions {
+  toolchainYarn: boolean;
+}
+
+export default class extends Generator<IOptions> {
   writing(): void {
     this.fs.copy(this.templatePath('.npmrc'), this.destinationPath('.npmrc'));
+  }
+
+  end(): void {
+    const initCommand = this.options.toolchainYarn ? 'yarn init' : 'npm init';
+    this.log(['', `✨  初始化完毕, 请通过 \`${chalk.red(initCommand)}\` 补充项目信息`, ''].join('\n'));
   }
 }
