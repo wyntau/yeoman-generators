5fe2bba8b27317c98783ad48f6f264d895c4367d
wyntau <github@wtliu.com>
2021-09-28 14:24:29 +0800

    feat(toolchain-prettier): 添加 ora, chalk

--- packages/generator-toolchain-prettier/src/app/index.ts
+++ packages/generator-toolchain-prettier/src/app/index.ts
@@ -1,8 +1,14 @@
 import Generator from 'yeoman-generator';
+import ora from 'ora';
+import chalk from 'chalk';
 
 export default class extends Generator {
   async writing(): Promise<void> {
     this.fs.copy(this.templatePath('.'), this.destinationPath('.'), { globOptions: { dot: true } });
-    await this.addDevDependencies(['prettier']);
+
+    const devDependencies = ['prettier'];
+    ora.promise(this.addDevDependencies(devDependencies), {
+      text: `Resolving package devDependencies ${chalk.red(devDependencies.join(', '))}`,
+    });
   }
 }
