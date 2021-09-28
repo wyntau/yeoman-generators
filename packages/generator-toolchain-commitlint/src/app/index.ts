6c446ca4ba56dbd64e3a1e5b1a14d760ff981487
wyntau <github@wtliu.com>
2021-09-28 12:58:07 +0800

    feat(toolchain-commitlint): use ora add loading spinner

--- packages/generator-toolchain-commitlint/src/app/index.ts
+++ packages/generator-toolchain-commitlint/src/app/index.ts
@@ -1,11 +1,18 @@
 import Generator from 'yeoman-generator';
 import { IGeneratorOptions } from '@wyntau/generator-shared';
+import ora from 'ora';
+import chalk from 'chalk';
 
 export default class extends Generator<IGeneratorOptions> {
   async writing(): Promise<void> {
     this.fs.copy(this.templatePath('.'), this.destinationPath('.'), {
       globOptions: { dot: true },
     });
-    await this.addDevDependencies(['@commitlint/cli', '@commitlint/config-conventional']);
+
+    const devDependencies = ['@commitlint/cli', '@commitlint/config-conventional'];
+
+    ora.promise(this.addDevDependencies(devDependencies), {
+      text: `Resolving package devDependencies ${chalk.red(devDependencies.join(', '))}`,
+    });
   }
 }
