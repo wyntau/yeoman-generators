7810eb515ee04a287e2f7509b5e2a05edff4d57a
wyntau <github@wtliu.com>
2021-09-28 14:26:44 +0800

    feat(toolchain-typescript): 添加 ora, chalk

--- packages/generator-toolchain-typescript/src/app/index.ts
+++ packages/generator-toolchain-typescript/src/app/index.ts
@@ -1,11 +1,19 @@
 import Generator from 'yeoman-generator';
 import chalk from 'chalk';
 import { IGeneratorOptions } from '@wyntau/generator-shared';
+import ora from 'ora';
 
 export default class extends Generator<IGeneratorOptions> {
   async writing(): Promise<void> {
-    await this.addDevDependencies(['typescript']);
-    await this.addDependencies(['tslib']);
+    const devDependencies = ['typescript'];
+    ora.promise(this.addDevDependencies(devDependencies), {
+      text: `Resolving package devDependencies ${chalk.red(devDependencies.join(', '))}`,
+    });
+
+    const dependencies = ['typescript'];
+    ora.promise(this.addDependencies(dependencies), {
+      text: `Resolving package dependencies ${chalk.red(dependencies.join(', '))}`,
+    });
   }
 
   end(): void {
