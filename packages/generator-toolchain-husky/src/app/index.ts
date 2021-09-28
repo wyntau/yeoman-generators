6bdb6134cb35e8e398a087d11fa03477fa55b3ca
wyntau <github@wtliu.com>
2021-09-28 14:20:08 +0800

    feat(toolchain-husky): add ora and chalk

--- packages/generator-toolchain-husky/src/app/index.ts
+++ packages/generator-toolchain-husky/src/app/index.ts
@@ -1,8 +1,14 @@
 import Generator from 'yeoman-generator';
+import ora from 'ora';
+import chalk from 'chalk';
 
 export default class extends Generator {
   async writing(): Promise<void> {
     this.fs.extendJSON(this.destinationPath('package.json'), { scripts: { prepare: 'husky install || exit 0' } });
-    await this.addDevDependencies(['husky']);
+
+    const devDependencies = ['husky'];
+    ora.promise(this.addDevDependencies(devDependencies), {
+      text: `Resolving package devDependencies ${chalk.red(devDependencies.join(', '))}`,
+    });
   }
 }
