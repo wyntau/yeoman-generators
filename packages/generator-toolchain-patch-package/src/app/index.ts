bf2259a90d801bc042ad7a87cc58661b18782911
wyntau <github@wtliu.com>
2021-09-28 14:23:15 +0800

    feat(toolchain-patch-package): 添加 ora, chalk

--- packages/generator-toolchain-patch-package/src/app/index.ts
+++ packages/generator-toolchain-patch-package/src/app/index.ts
@@ -1,10 +1,16 @@
 import Generator from 'yeoman-generator';
+import ora from 'ora';
+import chalk from 'chalk';
 
 export default class extends Generator {
   async writing(): Promise<void> {
     this.fs.extendJSON(this.destinationPath('package.json'), {
       scripts: { postinstall: 'patch-package', 'patch-package': "patch-package --exclude 'the-file-not-exist'" },
     });
-    await this.addDevDependencies(['patch-package', 'postinstall-postinstall']);
+
+    const devDependencies = ['patch-package', 'postinstall-postinstall'];
+    ora.promise(this.addDevDependencies(devDependencies), {
+      text: `Resolving package devDependencies ${chalk.red(devDependencies.join(', '))}`,
+    });
   }
 }
