6bbb022e8bb9f7a166ddf595bd771675a3c5f695
wyntau <github@wtliu.com>
2021-09-28 14:21:16 +0800

    feat(toolchain-lint-staged): add ora and chalk

--- packages/generator-toolchain-lint-staged/src/app/index.ts
+++ packages/generator-toolchain-lint-staged/src/app/index.ts
@@ -1,11 +1,17 @@
 import Generator from 'yeoman-generator';
 import { IGeneratorOptions } from '@wyntau/generator-shared';
+import ora from 'ora';
+import chalk from 'chalk';
 
 export default class extends Generator<IGeneratorOptions> {
   async writing(): Promise<void> {
     this.fs.copy(this.templatePath('.'), this.destinationPath('.'), {
       globOptions: { dot: true },
     });
-    await this.addDevDependencies(['lint-staged']);
+
+    const devDependencies = ['lint-staged'];
+    ora.promise(this.addDevDependencies(devDependencies), {
+      text: `Resolving package devDependencies ${chalk.red(devDependencies.join(', '))}`,
+    });
   }
 }
