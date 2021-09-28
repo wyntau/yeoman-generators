4a56ecdd5822d7b36ca9e6956c075e1e227e29da
wyntau <github@wtliu.com>
2021-09-28 11:27:06 +0800

    refactor(toolchain-npm): 更新复制内容, 通过 shared 展示提示信息

--- packages/generator-toolchain-npm/src/app/index.ts
+++ packages/generator-toolchain-npm/src/app/index.ts
@@ -5,10 +5,7 @@ import { IGeneratorOptions } from '@wyntau/generator-shared';
 export type IGeneratorToolchainNpmOptions = Pick<IGeneratorOptions, 'toolchainNpm'>;
 
 export default class GeneratorToolchainNpm extends Generator<IGeneratorToolchainNpmOptions> {
-  end(): void {
-    if (!this.options.toolchainNpm) {
-      return;
-    }
-    this.log(['', `✨  请通过 \`${chalk.red('npm init')}\` 补充项目信息`, ''].join('\n'));
+  writing(): void {
+    this.fs.copy(this.templatePath('.'), this.destinationPath('.'), { globOptions: { dot: true } });
   }
 }
