a8d1e0e15b62b19156161f1e3109de12afa519dd
liuwentao04 <liuwentao04@meituan.com>
2021-09-29 21:49:28 +0800

    refactor(toolchain-npm): update import

--- packages/generator-toolchain-npm/src/app/index.ts
+++ packages/generator-toolchain-npm/src/app/index.ts
@@ -1,10 +1,6 @@
 import Generator from 'yeoman-generator';
-import chalk from 'chalk';
-import { IGeneratorOptions } from '@wyntau/generator-shared';
 
-export type IGeneratorToolchainNpmOptions = Pick<IGeneratorOptions, 'toolchainNpm'>;
-
-export default class GeneratorToolchainNpm extends Generator<IGeneratorToolchainNpmOptions> {
+export default class GeneratorToolchainNpm extends Generator {
   writing(): void {
     this.fs.copy(this.templatePath('.'), this.destinationPath('.'), { globOptions: { dot: true } });
   }
