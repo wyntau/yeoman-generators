030d2644739c14140ea3160f6e988e0ebf7547b0
wyntau <github@wtliu.com>
2021-09-29 14:02:00 +0800

    feat(toolchain-lerna): support toolchain-lerna

--- packages/generator-toolchain-lerna/src/app/index.ts
+++ packages/generator-toolchain-lerna/src/app/index.ts
@@ -1,10 +1,37 @@
 import Generator from 'yeoman-generator';
 import ora from 'ora';
 import chalk from 'chalk';
+import { IGeneratorOptions, GeneratorOptions } from '@wyntau/generator-shared';
 
-export default class extends Generator {
+export type IGeneratorToolchainLernaOptions = Pick<IGeneratorOptions, 'toolchainYarn'>;
+
+export default class GeneratorToolchainLerna extends Generator<IGeneratorToolchainLernaOptions> {
+  initializing(): void {
+    this.option(GeneratorOptions.toolchainYarn.optionKey, {
+      type: Boolean,
+      description: GeneratorOptions.toolchainYarn.message,
+    });
+  }
+
+  async prompting(): Promise<void> {
+    const options = await this.prompt([
+      {
+        type: 'confirm',
+        name: GeneratorOptions.toolchainYarn.promptKey,
+        message: GeneratorOptions.toolchainYarn.message,
+        default: this.options.toolchainYarn,
+        when: this.options.toolchainYarn === null || this.options.toolchainYarn === undefined,
+      },
+    ]);
+    this.options = Object.assign({}, this.options, options);
+  }
+
   async writing(): Promise<void> {
-    this.fs.extendJSON(this.destinationPath('package.json'), { scripts: { prepare: 'husky install || exit 0' } });
+    const contextData = { npmClient: this.options.toolchainYarn ? 'yarn' : 'npm' };
+
+    this.fs.copyTpl(this.templatePath('.'), this.destinationPath('.'), contextData, undefined, {
+      globOptions: { dot: true },
+    });
 
     const devDependencies = ['lerna'];
     const spinner = ora(`Resolving devDependencies ${chalk.red(devDependencies.join(', '))}`).start();
