12954b4820a980647f73e6d7d2f95861f3e3228e
wyntau <github@wtliu.com>
2021-09-29 12:46:36 +0800

    refactor(toolchain-yarn): support toolchain-lerna option

--- packages/generator-toolchain-yarn/src/app/index.ts
+++ packages/generator-toolchain-yarn/src/app/index.ts
@@ -1,8 +1,30 @@
 import Generator from 'yeoman-generator';
+import { IGeneratorOptions, GeneratorOptions } from '@wyntau/generator-shared';
 
-export default class extends Generator {
+export type IGeneratorToolchainYarnOptions = Pick<IGeneratorOptions, 'toolchainLerna'>;
+
+export default class GeneratorToolchainYarn extends Generator<IGeneratorToolchainYarnOptions> {
   initializing(): void {
     this.env.options.nodePackageManager = 'yarn';
+
+    this.option(GeneratorOptions.toolchainLerna.message, {
+      type: Boolean,
+      description: GeneratorOptions.toolchainLerna.message,
+    });
+  }
+
+  async prompting(): Promise<void> {
+    const options = await this.prompt<IGeneratorToolchainYarnOptions>([
+      {
+        type: 'confirm',
+        name: GeneratorOptions.toolchainLerna.promptKey,
+        message: GeneratorOptions.toolchainLerna.message,
+        default: this.options.toolchainLerna ?? true,
+        when: this.options.toolchainLerna === undefined || this.options.toolchainLerna === null,
+      },
+    ]);
+
+    this.options = Object.assign(this.options, options);
   }
 
   writing(): void {
@@ -13,5 +35,9 @@ export default class extends Generator {
         npm: 'use-yarn-please',
       },
     });
+
+    if (this.options.toolchainLerna) {
+      this.fs.extendJSON(this.destinationPath('package.json'), { private: true, workspaces: ['packages/*'] });
+    }
   }
 }
