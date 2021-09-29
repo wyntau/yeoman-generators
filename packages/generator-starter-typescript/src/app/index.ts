84ca5bab3e026813ad431ebf5e6da918bf2118cf
wyntau <github@wtliu.com>
2021-09-29 10:49:01 +0800

    refactor(starter-typescript): 添加 log 信息

--- packages/generator-starter-typescript/src/app/index.ts
+++ packages/generator-starter-typescript/src/app/index.ts
@@ -1,6 +1,7 @@
 import Generator from 'yeoman-generator';
 import { IGeneratorOptions } from '@wyntau/generator-shared';
 import { IGeneratorToolchainEslintOptions } from '@wyntau/generator-toolchain-eslint';
+import chalk from 'chalk';
 
 export default class extends Generator<IGeneratorOptions> {
   default(): void {
@@ -24,5 +25,9 @@ export default class extends Generator<IGeneratorOptions> {
     this.composeWith(require.resolve('@wyntau/generator-toolchain-lint-staged/generators/app'));
 
     this.composeWith(require.resolve('@wyntau/generator-toolchain-patch-package/generators/app'));
+  }
+
+  end(): void {
+    this.log(`✨  请通过 \`${chalk.red('npx tsc --init')}\` 初始化 tsconfig.json`);
   }
 }
